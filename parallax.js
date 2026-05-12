153d88c5d6c5db56266b13220a22d291d1f704fe
Barys <boris.komarov@proton.me>
2026-05-13 00:34:32 +0200

    added particles, and fixed forgotten favicon

--- parallax.js
+++ parallax.js
@@ -4,11 +4,13 @@ function doParallax() {
 	const speed1 = 0.6;
 	const speed2 = 0.4;
 	const speed3 = 0.2;
+	const speed_petals = 0.5;
 
 	document.body.style.backgroundPosition = `
 		center ${-(scrolled * speed1)}px,
 		center ${-(scrolled * speed2)}px,
 		center ${-(scrolled * speed3)}px,
+		center ${-(scrolled * speed_petals)}px,
 		center center
 	`;
 }
